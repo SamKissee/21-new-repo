e9b722fba72021579701fd6075f810147bdeea10
Colin Worf and Sam Kissee <sam@>
2017-11-07 09:33:42 -0800

    add suit faces

--- js/scripts.js
+++ js/scripts.js
@@ -2,20 +2,22 @@ var masterDeck = []
 resetDeck();
 console.log(masterDeck);
 function resetDeck() {
-  function Cards(number, suit) {
-    this.number = number;
+  function Cards(value, face, suit) {
+    this.value = value
+    this.face = face;
     this.suit = suit;
-    this.image = number + suit + ".png";
+    this.image = face + suit + ".png";
   }
   var suits = ["hearts","spades", "diamonds", "clubs"]
   suits.forEach(function(suit){
     for (var i = 2; i <= 11; i++) {
       if (i === 10) {
-        for (var x = 0; x < 4; x++) {
-          masterDeck.push(new Cards (10, suit));
-        }
+          masterDeck.push(new Cards (10 ,"10", suit));
+          masterDeck.push(new Cards (10, "J", suit));
+          masterDeck.push(new Cards (10 ,"Q", suit));
+          masterDeck.push(new Cards (10, "K", suit));
       } else {
-          masterDeck.push(new Cards (i, suit));
+          masterDeck.push(new Cards (i, "" + i + "", suit));
         }
     }
   });
@@ -29,7 +31,6 @@ function Player(type, hand){
 }
 
 Player.prototype.resetPlayer = function(){
-  this.playerActive = false;
   this.playerHand = [];
   this.playerScore = 0;
   this.bust = false
@@ -38,7 +39,7 @@ Player.prototype.resetPlayer = function(){
 Player.prototype.scoreCalc = function(){
   var score = 0;
   for (var i = 0; i < this.playerHand.length; i++) {
-    score += this.playerHand[i].number;
+    score += this.playerHand[i].value;
   }
   if (score > 21) {
     this.bust = true;
@@ -58,32 +59,36 @@ Player.prototype.deal = function(x) {
 }
 
 $(function(){
-  // var players = [];
-  var Dealer = new Player("Dealer", []);
+
+  var dealer = new Player("Dealer", []);
   var newPlayer = new Player("Player", []);
-  // var howMany = parseInt($('#how-many').val());
-  // for (var i = 0; i < howMany; i++) {
-  //   players.push(newPlayer)
-  //   players[i].id = i;
-  // }
+
   $('#deal').click(function(){
     newPlayer.deal(2);
+    dealer.deal(2);
     newPlayer.scoreCalc();
     console.log(newPlayer.playerHand);
-    console.log(newPlayer.playerScore);
     if (newPlayer.bust === true){
       console.log("BUST!");
+      newPlayer.resetPlayer()
     };
   });
   $('#hit').click(function(){
     newPlayer.deal(1);
     newPlayer.scoreCalc();
     console.log(newPlayer.playerHand);
-    console.log(newPlayer.playerScore);
     if (newPlayer.bust === true){
       console.log("BUST!");
+      newPlayer.resetPlayer()
     };
   });
 
 
 });
+
+// var players = [];
+// var howMany = parseInt($('#how-many').val());
+// for (var i = 0; i < howMany; i++) {
+//   players.push(newPlayer)
+//   players[i].id = i;
+// }
